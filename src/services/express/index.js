874c7568d91b98c6ec5ec36d81bdb51aec2660e3
Ash Winter <ash_winter@hotmail.co.uk>
2019-05-10 20:14:18 +0100

    Finished structured logging part

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -2,9 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import compression from 'compression'
 import morgan from 'morgan'
-import rfs from 'rotating-file-stream'
 import bodyParser from 'body-parser'
-import path from 'path'
 import generateSafeId from 'generate-safe-id'
 import {errorHandler as queryErrorHandler} from 'querymen'
 import {errorHandler as bodyErrorHandler} from 'bodymen'
@@ -13,35 +11,36 @@ import {env} from '../../config'
 export default (apiRoot, routes) => {
 
   const app = express();
-  const logDirectory = path.join(__dirname, 'logs');
+  const json = require('morgan-json');
   const eventLookup = require('./eventLookup');
-  let accessLogStream = rfs('access.log', {
-    interval: '1d',
-    path: logDirectory
-  });
 
   /* istanbul ignore next */
   if (env === 'production' || env === 'development') {
-    app.use(cors())
+    app.use(cors());
     app.use(compression())
   }
 
-  app.use(morgan('dev', {
-    skip: function (req, res) {
-      return res.statusCode < 400
-    }
-  }));
+  morgan.token('id', function getId() {
+    return generateSafeId()
+  });
 
-  app.use(morgan(function (req, res) {
-    return `{ '${eventLookup.setEventName(req.originalUrl, req.method, res.statusCode)}' : 'timestamp': '${new Date().toISOString()}', 'id': '${generateSafeId()}', 'url' : '${req.originalUrl}', 'status' : '${res.statusCode}', 'method' : '${req.method} }`;
-  }, {stream: accessLogStream}));
+  morgan.token('eventName', function getEventName(req, res) {
+    return eventLookup.setEventName(req.originalUrl, req.method, res.statusCode);
+  });
 
-  app.use(bodyParser.urlencoded({extended: false}))
-  app.use(bodyParser.json())
-  app.use(apiRoot, routes)
-  app.use(queryErrorHandler())
-  app.use(bodyErrorHandler())
+  const format = json({
+    summary: '[:date[clf]] :id :eventName :method :url :status',
+    content: ':res[content-length]',
+    'response-time': ':response-time ms'
+  });
 
+  app.use(morgan(format));
+
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(apiRoot, routes);
+  app.use(queryErrorHandler());
+  app.use(bodyErrorHandler());
 
   return app
 }
--- src/services/mongoose/index.js
+++ src/services/mongoose/index.js
@@ -1,15 +1,13 @@
 import Promise from 'bluebird'
 import mongoose from 'mongoose'
 import {mongo} from '../../config'
-import path from "path";
-import rfs from "rotating-file-stream";
 import generateSafeId from 'generate-safe-id';
-
-const logDirectory = path.join(__dirname, 'logs');
 
 Object.keys(mongo.options).forEach((key) => {
   mongoose.set(key, mongo.options[key])
 });
+
+const json = require('morgan-json');
 
 mongoose.Promise = Promise;
 
@@ -24,32 +22,18 @@ mongoose.connection.on('error', (err) => {
 });
 
 mongoose.set('debug', function (coll, method, query, doc) {
-  let mongoEvent =
-    {
-      timeStamp: new Date().toISOString(),
-      id: generateSafeId(),
-      coll: coll,
-      method: method,
-      query: query,
-      doc: doc
-    };
+  let mongoEvent = {
+    timeStamp: new Date().toISOString(),
+    id: generateSafeId(),
+    coll: coll,
+    method: method,
+    query: query,
+    doc: doc
+  };
 
-let mongoLogStream = rfs('mongo.log', {
-  interval: '1d',
-  path: logDirectory,
-
-});
-
-console.info({
-  mongoEvent: mongoEvent
-});
-
-mongoLogStream.on("error", (err) => {
-  console.log("RFS error", err);
-});
-
-mongoLogStream.write(JSON.stringify(mongoEvent));
-
+  console.info({
+    mongoEvent: mongoEvent
+  });
 });
 
 export default mongoose
